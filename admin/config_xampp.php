09f4f3ec92f8ff34585abeec22428f841f4adc82
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2012-04-25 19:05:46 +0000

    environment configs

---
+++ admin/config_xampp.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+define('HTTP_IMAGE', 'http://localhost/opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_IMAGE', 'http://localhost/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/opencart/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/opencart/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/opencart/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/opencart/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/opencart/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/opencart/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/opencart/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/opencart/system/logs/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', '8EYv466ZxA8uWEG4');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PREFIX', '');
+?>
