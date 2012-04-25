f453bbd131f1ab7a28477b205078e46616bd1e52
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2012-04-25 18:53:25 +0000

    environment configs

---
+++ config_fredandmatt.com.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.fredandmatt.com/opencart/');
+define('HTTP_IMAGE', 'http://www.fredandmatt.com/opencart/image/');
+define('HTTP_ADMIN', 'http://www.fredandmatt.com/opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.fredandmatt.com/opencart/');
+define('HTTPS_IMAGE', 'http://www.fredandmatt.com/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/bitnami/apache2/htdocs/opencart/catalog/');
+define('DIR_SYSTEM', '/opt/bitnami/apache2/htdocs/opencart/system/');
+define('DIR_DATABASE', '/opt/bitnami/apache2/htdocs/opencart/system/database/');
+define('DIR_LANGUAGE', '/opt/bitnami/apache2/htdocs/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/opt/bitnami/apache2/htdocs/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/bitnami/apache2/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/opt/bitnami/apache2/htdocs/opencart/image/');
+define('DIR_CACHE', '/opt/bitnami/apache2/htdocs/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/opt/bitnami/apache2/htdocs/opencart/download/');
+define('DIR_LOGS', '/opt/bitnami/apache2/htdocs/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', '8EYv466ZxA8uWEG4');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PREFIX', '');
+?>
